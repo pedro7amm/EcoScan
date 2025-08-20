8210230b3437633409e93161ff9eef20293b5f72
Pedro Meléndez <pedrolendez7@gmail.com>
2025-08-19 20:33:03 -0600

    Creating dashboard page

---
+++ src/app/dashboard/dashboard.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonicModule],
+  templateUrl: './dashboard.page.html',
+  styleUrls: ['./dashboard.page.scss'],
+})
+export class DashboardPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
